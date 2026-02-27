9c824173b8a0bc75f7146d5ebab30a7480d7fc00
mubumubashir925-star <mubumubashir925@gmail.com>
2026-02-27 23:17:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sahithyotsav-v1.0';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install - cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate - clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch - serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
